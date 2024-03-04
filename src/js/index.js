c84f496f2450514f8defde2b3e14580b1cefe573
Mr-No-bo-dy <o.m.gutsalyuk@gmail.com>
2024-03-04 15:55:06 +0200

    v1.6 - Hamburger updated

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,17 @@
+// Hamburger (simple)
 let hamburger = document.querySelector(".hamburger")
-let menu = document.querySelector(".menu")
-
+let nav = document.querySelector(".nav-menu")
 hamburger.onclick = function () {
-  menu.classList.toggle ("active-burger")
+  nav.classList.toggle ("adapted-nav")
   hamburger.classList.toggle ("hamburger-open")
 }
+
+// // Hamburger
+// let hamburger = document.querySelector(".hamburger");
+// let nav = document.querySelector(".nav-menu");
+// hamburger.addEventListener("click", (function () {
+//    hamburger.classList.toggle("hamburger-open"),
+//    hamburger.classList.contains("hamburger-open")
+//    ? (nav.style.display = "block", nav.classList.add("adapted-nav"))
+//    : (nav.style.display = "none", nav.removeAttribute("style"), nav.classList.remove("adapted-nav"))
+// }));
